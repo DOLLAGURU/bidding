a0bdb62b9e9bc573e965913ef63ab704f0440655
DOLLAGURU <59775677+DOLLAGURU@users.noreply.github.com>
2022-09-27 22:04:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
                     </div>
                     <div class="col-lg-4 mr-auto text-center">
                         <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
-                        <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                        <!-- Make sure to change the email address in BOTH the anchor text and the link target below the code!-->
                         <a class="d-block" href="mailto:<?php echo $_SESSION['system']['email'] ?>"><?php echo $_SESSION['system']['email'] ?></a>
                     </div>
                 </div>
